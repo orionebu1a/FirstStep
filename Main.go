fec2e19f268ba51cbbb915c541431d8ac116b4ad
Alex <mcs666554@gmail.com>
2023-07-20 13:55:15 +0300

    initial

--- Main.go
+++ Main.go
@@ -28,48 +28,44 @@ CREATE TABLE theme (
     content text
 )`
 
-
-
 type Course struct {
-	primary_id	int64		`db:"primary_id"`
-	owner_id	int64		`db:"owner_id"`
-	info		string		`db:"info"`
-	section_id	[]int64		`db:"section_id"`
+	primary_id int64   `db:"primary_id"`
+	owner_id   int64   `db:"owner_id"`
+	info       string  `db:"info"`
+	section_id []int64 `db:"section_id"`
 }
 
 type Section struct {
-	primary_id	int64		`db:"primary_id"`
-	course_id	int64		`db:"course_id"`
-	themes_id	[]int64		`db:"themes_id"`
+	primary_id int64   `db:"primary_id"`
+	course_id  int64   `db:"course_id"`
+	themes_id  []int64 `db:"themes_id"`
 }
 
-type Theme struct{
-	primary_id	int64		`db:"primary_id"`
-	section_id	int64		`db:"section_id"`
-	content		string		`db:"content"`
+type Theme struct {
+	primary_id int64  `db:"primary_id"`
+	section_id int64  `db:"section_id"`
+	content    string `db:"content"`
 }
 
-func createTh(ctx context.Context, newTheme Theme){
+func createTh(ctx context.Context, newTheme Theme) {
 	tx := ctx.Value("tx").(*sqlx.Tx)
 	tx.Query("INSERT INTO themes VALUES ($1, $2, $3);", newTheme.primary_id, newTheme.section_id, newTheme.content)
 }
 
-func createTheme(ctx context.Context, newTheme Theme){
+func createTheme(ctx context.Context, newTheme Theme) {
 	tx := ctx.Value("tx").(*sqlx.Tx)
 	tx.Query("INSERT INTO themes VALUES ($1, $2, $3);", newTheme.primary_id, newTheme.section_id, newTheme.content)
 }
 
-func readTheme(ctx context.Context, themeId int64) Theme{
+func readTheme(ctx context.Context, themeId int64) Theme {
 	tx := ctx.Value("tx").(*sqlx.Tx)
 	var primaryId, sectionId int64
 	var content string
-	query,_ := tx.Query("SELECT * from theme WHERE primary_id = $1;", themeId)
+	query, _ := tx.Query("SELECT * from theme WHERE primary_id = $1;", themeId)
 	query.Next()
 	query.Scan(&primaryId, &sectionId, &content)
 	return Theme{primary_id: primaryId, section_id: primaryId, content: content}
 }
-
-
 
 func main() {
 	db, err := sqlx.Open("postgres", "host=localhost dbname=habrdb sslmode=disable user=habrpguser password=pgpwd4habr")
@@ -83,7 +79,3 @@ func main() {
 
 	fmt.Println(theme.content)
 }
-
-
-
-
